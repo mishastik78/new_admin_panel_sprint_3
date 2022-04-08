11ecee4160fb03d34c03a121d69b09a829370568
mishastik78 <mishastik78@yande.ru>
2022-04-08 08:36:44 +0300

    second review

--- 01_etl/loaders.py
+++ 01_etl/loaders.py
@@ -47,8 +47,120 @@ class PostgreDb:
 
 
 class EsDb:
+    settings = {
+        "settings": {
+            "refresh_interval": "1s",
+            "analysis": {
+                "filter": {
+                    "english_stop": {
+                        "type": "stop",
+                        "stopwords": "_english_"
+                    },
+                    "english_stemmer": {
+                        "type": "stemmer",
+                        "language": "english"
+                    },
+                    "english_possessive_stemmer": {
+                        "type": "stemmer",
+                        "language": "possessive_english"
+                    },
+                    "russian_stop": {
+                        "type": "stop",
+                        "stopwords": "_russian_"
+                    },
+                    "russian_stemmer": {
+                        "type": "stemmer",
+                        "language": "russian"
+                    }
+                },
+                "analyzer": {
+                    "ru_en": {
+                        "tokenizer": "standard",
+                        "filter": [
+                            "lowercase",
+                            "english_stop",
+                            "english_stemmer",
+                            "english_possessive_stemmer",
+                            "russian_stop",
+                            "russian_stemmer"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+    mappings = {
+        "mappings": {
+            "dynamic": "strict",
+            "properties": {
+                "id": {
+                    "type": "keyword"
+                },
+                "imdb_rating": {
+                    "type": "float"
+                },
+                "genre": {
+                    "type": "keyword"
+                },
+                "title": {
+                    "type": "text",
+                    "analyzer": "ru_en",
+                    "fields": {
+                        "raw": {
+                            "type": "keyword"
+                        }
+                    }
+                },
+                "description": {
+                    "type": "text",
+                    "analyzer": "ru_en"
+                },
+                "director": {
+                    "type": "text",
+                    "analyzer": "ru_en"
+                },
+                "actors_names": {
+                    "type": "text",
+                    "analyzer": "ru_en"
+                },
+                "writers_names": {
+                    "type": "text",
+                    "analyzer": "ru_en"
+                },
+                "actors": {
+                    "type": "nested",
+                    "dynamic": "strict",
+                    "properties": {
+                        "id": {
+                            "type": "keyword"
+                        },
+                        "name": {
+                            "type": "text",
+                            "analyzer": "ru_en"
+                        }
+                    }
+                },
+                "writers": {
+                    "type": "nested",
+                    "dynamic": "strict",
+                    "properties": {
+                        "id": {
+                            "type": "keyword"
+                        },
+                        "name": {
+                            "type": "text",
+                            "analyzer": "ru_en"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     def __init__(self, path: str):
         self.es = Elasticsearch(path)
+        if not self.es.indices.exists(index='movies'):
+            self.es.indices.create(index='movies', mappings=self.mappings, settings=self.settings)
 
     def build_index(self, data: list[MoviesForEs]) -> list[dict]:
         return [{"_index": "movies", "_source": row.json()} for row in data]
